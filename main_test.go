38415d51634c9617e50d55ac5c34b93a44fa8c97
rodrilp <rodrigolp7@gmail.com>
2024-04-16 11:55:46 -0500

    Add test

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "net/http"
+  "net/http/httptest"
+  "testing"
+
+  "github.com/stretchr/testify/assert"
+)
+
+func TestBasicRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/", nil)
+	router.ServeHTTP(w, req)
+  
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "{\"data\":\"hello world\"}", w.Body.String())
+}
+
+func TestNoRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/api", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 404, w.Code)
+	assert.Equal(t, "{\"message\":\"Page not found\"}", w.Body.String())
+}
